f82d7bb8bbec7768e4a352efd15b4e3ae3a854d3
mvolchenkova <121573011+mvolchenkova@users.noreply.github.com>
2024-10-08 10:42:19 +0300

    add freelancer class

---
+++ freelancer_back/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace freelancer_back.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
