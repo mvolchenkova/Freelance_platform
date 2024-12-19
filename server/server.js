dbd4f330623b6dd54eab863d7425276c88f85025
Александр Шаройко <himanru3@gmail.com>
2024-12-19 15:37:07 +0300

    feat: added models

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express')
+const sequelize = require('./db.js');
+const PORT = process.env.PORT;
+const routes = require('./routes/index.js')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Соединение с базой данных успешно!');
+        await sequelize.sync(); 
+        app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}`);
+        });
+    } catch (e) {
+        console.error('Ошибка при подключении к базе данных:', e);
+    }
+  };
+  start();
