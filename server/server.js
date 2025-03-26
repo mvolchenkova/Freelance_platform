ecda7151eab07fd89b1d8c4eccda6a22817b0554
OneMight <dimamirankov63@gmail.com>
2025-03-26 18:18:14 +0300

    fix: fix server

--- server/server.js
+++ server/server.js
@@ -1,27 +1,14 @@
-const dotenv = require('dotenv');
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const sequelize = require('./db.js');
 const routes = require('./routes/index.js');
 
-// Получаем роль из переменной окружения
-const role = process.env.ROLE || 'customer';  // По умолчанию роль 'customer'
-
-// Загружаем соответствующий файл .env для роли
-dotenv.config({ path: `.env${role}` });
-
-console.log(`Пароль для роли ${role}: ${process.env.DB_PASSWORD}`);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-
-const PORT = process.env.PORT || 5000;  // Используем порт из .env или 5000 по умолчанию
+const PORT = process.env.PORT || 5000;  
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',  // Замените на адрес вашего фронтенда
+    origin: 'http://localhost:3000',
     credentials: true
 }));
 app.use(express.json());
